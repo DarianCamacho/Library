e20ef9ddfcb2b0d0273503dd751b9c22c498f7fa
Darian Camacho <delgadodarian2003@gmail.com>
2023-04-26 19:58:33 -0600

    Agregar archivos de proyecto.

---
+++ Database/DatabaseHelper.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace Library.Database
+{
+    public class DatabaseHelper
+    {
+        const string server = @"LAPTOP-J571RIQN";
+        const string database = "LibreriaInternacional";
+        private static string connectionString = string.Format("Data Source={0};Initial Catalog={1};Integrated Security=True", server, database);
+
+        //select
+        public static DataTable ExecuteQuery(string procedureName, List<SqlParameter> param)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.Connection = connection;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.CommandText = procedureName;
+
+                    if (param != null)
+                    {
+                        foreach (SqlParameter p in param)
+                        {
+                            cmd.Parameters.Add(p);
+                        }
+                    }
+
+                    cmd.ExecuteNonQuery();
+                    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+                    return dt;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+        //update - delete - insert
+        public static void ExecuteNonQuery(string procedureName, List<SqlParameter> param)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.Connection = connection;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.CommandText = procedureName;
+
+                    if (param != null)
+                    {
+                        foreach (SqlParameter p in param)
+                        {
+                            cmd.Parameters.Add(p);
+                        }
+                    }
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
